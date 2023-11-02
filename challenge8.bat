131b83a730d89dae1cad23f3511b7e3d9667944b
Kevin Hoang <hoang.kevin67@gmail.com>
2023-11-01 20:57:35 -0700

    Create challenge8.bat

---
+++ challenge8.bat
@@ -0,0 +1,29 @@
+
+#gg
+@echo off
+setlocal enabledelayedexpansion
+
+set /p sourcePath=Enter the source folder path:
+
+set /p destinationPath=Enter the destination folder path:
+
+if not exist "!sourcePath!\" (
+    echo Error: Source folder does not exist.
+    goto :eof
+)
+
+if not exist "!destinationPath!\" (
+    echo Error: Destination folder does not exist.
+    goto :eof
+)
+
+robocopy "!sourcePath!" "!destinationPath!" /E
+
+if errorlevel 8 (
+    echo Error: ROBOCOPY encountered errors during the copy operation.
+) else (
+    echo Copy operation completed successfully.
+)
+
+:end
+endlocal
