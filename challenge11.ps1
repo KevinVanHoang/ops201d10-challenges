2ae1362a29bf65d6fbe7934f5ceff4c67057170e
Kevin Hoang <hoang.kevin67@gmail.com>
2023-11-12 21:15:46 -0800

    Update challenge11.ps1

--- challenge11.ps1
+++ challenge11.ps1
@@ -8,10 +8,12 @@
 
 #Write a Powershell script that automates the configuration of a new Windows 10 endpoint. Your script should perform the following:
 # Enable File and Printer Sharing
-
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+Set-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Enabled True
 
 # Allow ICMP traffic
 Enable-NetFirewallRule -Name "FPS-ICMP4-ERQ-In"
+netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4
 
 # Enable Remote management
 Set-ExecutionPolicy RemoteSigned -Force
@@ -19,6 +21,15 @@ Enable-PSRemoting -Force
 
 # Remove bloatware
 Get-AppxPackage -AllUsers | Where-Object { $_.Name -like "*bloatware*" } | Remove-AppxPackage -AllUsers
+# Basil's solution
+iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
+
+
 # Enable Hyper-V
+Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 
 # Disable SMBv1, an insecure protocol
+# check if SMBv1 is enabled
+Get-SmbServerConfiguration 
+Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
